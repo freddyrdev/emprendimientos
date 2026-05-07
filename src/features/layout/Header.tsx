1c859b92379e0011f380c8ef6e3d12dce800e703
freddyrdev <freddyrdev@gmail.com>
2026-05-06 19:04:52 -0500

    Creacion del header de la pagina

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router"
+
 export const Header = () => {
     return (
-    <header>
-        
+    <header className="border-b border-(--bordeOscuro) px-50 py-5">
+        <nav className="flex gap-10 negrita">
+            <Link to={""} className="hover:text-(--temaPrimario) animacion">Inicio</Link>
+            <Link to={""} className="hover:text-(--temaPrimario) animacion">Categorias</Link>
+            <Link to={""} className="hover:text-(--temaPrimario) animacion">Emprendimientos</Link>
+            <Link to={""} className="hover:text-(--temaPrimario) animacion">Contactos</Link>
+        </nav>
     </header>
     )
 }
